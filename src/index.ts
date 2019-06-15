d6bc0e60c5a73978d5b01ba80e4a5d77c7f6f308
ngocthanh.nguyen <ngocthanh.nguyen2297@gmail.com>
2019-06-15 22:30:16 +0700

    feat(*): use env

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,26 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
 import * as Koa from "koa";
-import * as Router from 'koa-router';
+import * as Router from "koa-router";
 
 const app = new Koa();
 const router = new Router();
 
 import "reflect-metadata";
-import {createConnection} from "typeorm";
+import { createConnection } from "typeorm";
 
-createConnection().then(async connection => {
+createConnection()
+  .then(async connection => {
     console.log("Database connected!");
-    router.get('/*', async (ctx) => {
-        ctx.body = 'Hello World!';
+    router.get("/*", async ctx => {
+      ctx.body = "Hello World!";
     });
-    
+
     app.use(router.routes());
-    
-    app.listen(3000);
-    
-    console.log('Server running on port 3000');
-}).catch(error => console.log(error));
+
+    const PORT = process.env.PORT || 3000;
+
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch(error => console.log(error));
